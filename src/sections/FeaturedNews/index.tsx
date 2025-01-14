e2b15fc0584b6aae9568bf72d363dd6a7d32bd2c
Arthur Amaral <arthur.ramos.amaral@hotmail.com>
2025-01-14 11:00:13 -0300

    added title to featured news section

--- src/sections/FeaturedNews/index.tsx
+++ src/sections/FeaturedNews/index.tsx
@@ -4,15 +4,21 @@ import { NewsCard } from 'components/newsCard'
 import Mill from "assets/images/mill.png"
 import PeopleWithSuits from "assets/images/peopleWithSuits.png"
 import Bottles from "assets/images/bottles.png"
+import styles from "./styles.module.scss"
 
 export function FeaturedNews() {
 	return (
 
 		<ContentSection sectionBackground='almostWhiteBackground'>
-			<ContentContainer align='horizontalAlignCenter'>
-				<NewsCard category='research paper' imageAlt='Mill' imageSrc={Mill} title='Suspendisse lectus iaculis odio netus' />
-				<NewsCard category='blog' imageAlt='Plastic Bottles' imageSrc={Bottles} title='Egestas ut pretium tortor dolor lobortis vitae' />
-				<NewsCard category='blog' imageAlt='People With Suit' imageSrc={PeopleWithSuits} title='Pharetra donec ultrices dictumst ' />
+			<ContentContainer>
+				<div className={styles.wrapper}>
+					<h2 className={styles.title}>Featured News</h2>
+					<div className={styles.cards}>
+						<NewsCard category='research paper' imageAlt='Mill' imageSrc={Mill} title='Suspendisse lectus iaculis odio netus' />
+						<NewsCard category='blog' imageAlt='Plastic Bottles' imageSrc={Bottles} title='Egestas ut pretium tortor dolor lobortis vitae' />
+						<NewsCard category='blog' imageAlt='People With Suit' imageSrc={PeopleWithSuits} title='Pharetra donec ultrices dictumst ' />
+					</div>
+				</div>
 			</ContentContainer>
 		</ContentSection>
 	)
