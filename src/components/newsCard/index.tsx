20f82803f460c1df53ebdff400959a9ec22690ef
Arthur Amaral <arthur.ramos.amaral@hotmail.com>
2025-02-11 15:51:14 -0300

    correcting itens 1 2 4 7 and 8

--- src/components/newsCard/index.tsx
+++ src/components/newsCard/index.tsx
@@ -11,7 +11,9 @@ type NewsCardProps = {
 export const NewsCard: React.FC<NewsCardProps> = ({ category, imageSrc, title, imageAlt }) => {
 	return (
 		<div className={styles.wrapper}>
-			<img src={imageSrc} alt={imageAlt} className={styles.contentImage} />
+			<div className={styles.imageContainer}>
+				<img src={imageSrc} alt={imageAlt} className={styles.zoomImage} />
+			</div>
 			<Spacer height='16px' />
 			<span className={styles.category}>{category}</span>
 			<Spacer height='8px' />
