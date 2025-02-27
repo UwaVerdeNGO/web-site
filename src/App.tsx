84b06356b6e8a0fb65a34bf6c6361fc8b41a9b6c
Arthur Amaral <arthur.ramos.amaral@hotmail.com>
2025-02-27 16:01:26 -0300

    test removing comments

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { HorizontalCards } from 'sections/HorizontalCards';
 import { OurValues } from 'sections/OurValues';
 import { ImageSection } from 'sections/ImageSection';
 import { FeaturedNews } from 'sections/FeaturedNews';
-import { Comments } from 'sections/Coments';
+// import { Comments } from 'sections/Coments';
 import { ClimateIsChanging } from 'sections/ClimateIsChanging';
 import { Hero } from 'sections/Hero';
 import { WhatWeDo } from 'sections/WhatWeDo';
@@ -25,7 +25,7 @@ function App() {
 			<ImageSection />
 			<OurProjects />
 			<FeaturedNews />
-			<Comments comment={`"Learn as you grow, grow as you learn – That's what UwaVerde offers. It's a chance to be part of a meaningful effort for a sustainable future, while simultaneously growing knowledge and skills through collaborative learning."`} commentatorName='Silvia Prodanova' commentatorPosition='CCO' profilePhoto={Silvia} />
+			{/* <Comments comment={`"Learn as you grow, grow as you learn – That's what UwaVerde offers. It's a chance to be part of a meaningful effort for a sustainable future, while simultaneously growing knowledge and skills through collaborative learning."`} commentatorName='Silvia Prodanova' commentatorPosition='CCO' profilePhoto={Silvia} /> */}
 			<ClimateIsChanging />
 			<Newsletter />
 			<Footer />
