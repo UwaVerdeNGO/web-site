48e6dbf6ede1dd390a36067a0e673ff574e222c6
Subaga <subaga98@gmail.com>
2025-09-24 16:05:56 +0200

    Initial commit

---
+++ src/pages/MemberPortal/index.tsx
@@ -0,0 +1,9 @@
+import { HeaderMemberPortal } from "components/headerMemberPortal";
+
+
+function MemberPortal() {
+    return(<>
+    <HeaderMemberPortal/>
+    </>)
+}
+export default MemberPortal;
