783dfb69354a3d17208299c53cea21f5b88fbde2
Subaga <subaga98@gmail.com>
2025-11-11 14:16:55 +0100

    Change our values section to table format

--- src/sections/OurValues/index.tsx
+++ src/sections/OurValues/index.tsx
@@ -1,14 +1,22 @@
-import { AccordionComponent } from 'components/accordion';
-import { ContentSection } from 'components/contentSection';
-import { ContentContainer } from 'components/contentSection/contentContainer';
-import { AccordionData } from './accordionData';
+import { TableData } from './tableData';
+import styles from './styles.module.scss';
 
 export function OurValues() {
-	return (
-		<ContentSection sectionBackground='almostWhiteBackground'>
-			<ContentContainer>
-				<AccordionComponent data={AccordionData} />
-			</ContentContainer>
-		</ContentSection>
-	);
+  let data = TableData;
+  return (
+    <>
+      <div className={styles.ourValueContainer}>
+        <h2 className={styles.valueTitle}>Our Values</h2>
+        {data.map((item) => (
+          <table className={styles.table}>
+            <tr className={styles.tableRow}>
+              <td className={styles.bulletNum}>{item.id}</td>
+              <td className={styles.headerData}>{item.header}</td>
+              <td className={styles.bodyData}>{item.body}</td>
+            </tr>
+          </table>
+        ))}
+      </div>
+    </>
+  );
 }
---
+++ src/sections/OurValues/tableData.ts
@@ -0,0 +1,17 @@
+export const TableData = [
+  {
+    id: '01',
+    header: 'Environmental Stewardship',
+    body: "Environmental stewardship is more than just a concept,it's a call to action. Our projects are designed to empower individuals and organizations to take concrete steps towards a more sustainable future. ",
+  },
+  {
+	id: '02',
+    header: 'Collaboration',
+    body: 'We foster a culture of partnership and teamwork, working closely with individuals, organizations, and businesses to create a more sustainable future.',
+  },
+  {
+    id: '03',
+	header: 'Hands-on Learning',
+    body: 'We prioritise hands-on learning and practical experience as essential for empowering individuals to transition to sustainability careers while driving sustainable change.',
+  },
+];
