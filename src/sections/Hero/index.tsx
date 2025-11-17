8ad4aa93864a3d54426da6e84ea6a1241253f131
Subaga <subaga98@gmail.com>
2025-11-17 16:43:13 +0100

    Change hero section

--- src/sections/Hero/index.tsx
+++ src/sections/Hero/index.tsx
@@ -1,30 +1,22 @@
-import { ContentSection } from 'components/contentSection'
-import { ContentContainer } from 'components/contentSection/contentContainer'
-import Terra from "assets/images/terraTeste.png"
-import styles from "./styles.module.scss"
-import { Button } from 'components/buttons/button'
+import { ContentSection } from 'components/contentSection';
+import { ContentContainer } from 'components/contentSection/contentContainer';
+import Terra from 'assets/images/terraTeste.png';
+import styles from './styles.module.scss';
+import { Button } from 'components/buttons/button';
+import HeroBg from 'assets/images/hero-1920.png';
 
 export function Hero() {
-	return (
-		<ContentSection sectionBackground='almostWhiteBackground'>
-			<ContentContainer>
-				<div className={styles.contentWrapper}>
-
-					<div className={styles.mobileButton}>
-						<Button background='gradient' fontSize='medium' href='#' spacing="mediumSpacing">Join Our Mission</Button>
-					</div>
-
-					<div className={styles.leftContent}>
-						<h2>Network, Innovate, Sustain.</h2>
-						<h3> Promote sustainable living through eco-friendly products and innovative solutions.</h3>
-					</div>
-					<div className={styles.rightContent}>
-						<img src={Terra} alt="Earth Content" />
-					</div>
-
-				</div>
-			</ContentContainer>
-		</ContentSection>
-	)
+  return (
+    <div className={styles.contentWrapper}>
+      <div className={styles.heroImgContainer}>
+        <p className={styles.heroPara}>
+          Promote sustainable living through eco- friendly products and
+          innovative solutions.
+        </p>
+        <p className={styles.heroHeading}>
+          hub for young minds and green action
+        </p>
+      </div>
+    </div>
+  );
 }
-
