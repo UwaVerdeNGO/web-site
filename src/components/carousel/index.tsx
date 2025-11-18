2a404e7b09e09e5364830be835ba4383f4f0ac82
Subaga <subaga98@gmail.com>
2025-11-18 19:43:52 +0100

    Change our projects section

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -1,70 +1,93 @@
-import React, { useState } from "react";
-import GreenArrow from "assets/icons/greenArrow.png"
-import WhiteArrow from "assets/icons/whiteArrow.png"
-import styles from "./styles.module.scss"
+import React, { useState } from 'react';
+import PurpleArrow from 'assets/icons/purpleArrow.png';
+import WhiteArrow from 'assets/icons/whiteArrow.png';
+import styles from './styles.module.scss';
 
 interface CarouselProps {
-	cards: React.ReactNode[]; // Aceita componentes de card prontos
+  cards: React.ReactNode[]; // Aceita componentes de card prontos
 }
 
 export const Carousel: React.FC<CarouselProps> = ({ cards }) => {
-	const [activeIndex, setActiveIndex] = useState(0);
-	const [isFowardHovered, setIsFowardHovered] = useState(false);
-	const [isBackwardHovered, setIsBackwardHovered] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [isFowardHovered, setIsFowardHovered] = useState(false);
+  const [isBackwardHovered, setIsBackwardHovered] = useState(false);
 
-	const handlePrev = () => {
-		if (activeIndex === 0) {
-			return setActiveIndex(cards.length - 1)
-		}
-		return setActiveIndex(activeIndex - 1)
-	};
+  const handlePrev = () => {
+    if (activeIndex === 0) {
+      return setActiveIndex(cards.length - 1);
+    }
+    return setActiveIndex(activeIndex - 1);
+  };
 
-	const handleNext = () => {
-		if (activeIndex === cards.length - 1) {
-			return setActiveIndex(0)
-		}
-		return setActiveIndex(activeIndex + 1)
-	};
+  const handleNext = () => {
+    if (activeIndex === cards.length - 1) {
+      return setActiveIndex(0);
+    }
+    return setActiveIndex(activeIndex + 1);
+  };
 
+  const getClassName = (index: number) => {
+    if (index === activeIndex) {
+      return styles.active;
+    }
+    if (index === activeIndex - 1) {
+      return styles.prev;
+    }
+    if (index === activeIndex + 1) {
+      return styles.next;
+    }
+    if (activeIndex === 0 && index === cards.length - 1) {
+      return styles.prev;
+    }
+    if (activeIndex === cards.length - 1 && index === 0) {
+      return styles.next;
+    }
+    return styles.hidden;
+  };
 
-	const getClassName = (index: number) => {
-		if (index === activeIndex) {
-			return styles.active;
-		}
-		if (index === activeIndex - 1) {
-			return styles.prev
-		}
-		if (index === activeIndex + 1) {
-			return styles.next
-		}
-		if (activeIndex === 0 && index === cards.length - 1) {
-			return styles.prev
-		}
-		if (activeIndex === cards.length - 1 && index === 0) {
-			return styles.next
-		}
-		return styles.hidden
-	};
-
-	return (
-		<div className={styles.carouselContainer}>
-			<div className={styles.buttonsWrapper}>
-				<button onClick={handlePrev} className={styles.carouselButton} onMouseEnter={() => setIsBackwardHovered(true)} onMouseLeave={() => setIsBackwardHovered(false)}>
-					<img src={isBackwardHovered ? WhiteArrow : GreenArrow} alt="Back Arrow" />
-				</button>
-				<button onClick={handleNext} className={styles.carouselButton} onMouseEnter={() => setIsFowardHovered(true)} onMouseLeave={() => setIsFowardHovered(false)}>
-					<img src={isFowardHovered ? WhiteArrow : GreenArrow} alt="Foward Arrow" />
-
-				</button>
-			</div>
-			<div className={styles.carouselCards}>
-				{cards.map((item, index) => (
-					<div key={index} className={`${styles.carouselCard} ${getClassName(index)}`}>
-						{item}
-					</div>
-				))}
-			</div>
-
-		</div>
-	);
+  return (
+    <div className={styles.carouselContainer}>
+      <div className={styles.cardsIndex}>
+        {cards.map((item, index) => (
+          <p className={`${styles.cardIndex} ${getClassName(index)}`}>
+            0{index + 1} /
+          </p>
+        ))}
+      </div>
+      <div className={styles.buttonsWrapper}>
+        <button
+          onClick={handlePrev}
+          className={styles.carouselButton}
+          onMouseEnter={() => setIsBackwardHovered(true)}
+          onMouseLeave={() => setIsBackwardHovered(false)}
+        >
+          <img
+            src={isBackwardHovered ? WhiteArrow : PurpleArrow}
+            alt="Back Arrow"
+          />
+        </button>
+        <button
+          onClick={handleNext}
+          className={styles.carouselButton}
+          onMouseEnter={() => setIsFowardHovered(true)}
+          onMouseLeave={() => setIsFowardHovered(false)}
+        >
+          <img
+            src={isFowardHovered ? WhiteArrow : PurpleArrow}
+            alt="Foward Arrow"
+          />
+        </button>
+      </div>
+      <div className={styles.carouselCards}>
+        {cards.map((item, index) => (
+          <div
+            key={index}
+            className={`${styles.carouselCard} ${getClassName(index)}`}
+          >
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
--- src/sections/OurProjects/index.tsx
+++ src/sections/OurProjects/index.tsx
@@ -1,47 +1,72 @@
 import { Carousel } from 'components/carousel';
 import { Card } from 'components/card';
-import styles from "./styles.module.scss"
+import styles from './styles.module.scss';
 
 export function OurProjects() {
-	const cards = [
-		<Card background='transparent' spacing='mediumSpacing'>
-			<div className={styles.cardContent}>
-				<h2 className={styles.cardTitle}>Green Building</h2>
-				<span className={styles.cardDescription}>
-					We are aiming to reduce buildings' carbon footprint by utilizing advanced insulation technologies, sustainable concrete, and incorporating waste materials in construction. We seek to promote a greener, more sustainable future and foster a circular economy with waste being a valuable resource.
-				</span>
-				<a href="#" role='button' className={styles.cardButton}>
-					See More
-				</a>
-			</div>
-		</Card>,
-		<Card background='transparent' spacing='mediumSpacing'>
-			<div className={styles.cardContent}>
-				<h2 className={styles.cardTitle}>Agro Innovative</h2>
-				<span className={styles.cardDescription}>
-					We are developing a farm suite integrated with IoT to help farmers optimize their farm processes, reduce carbon footprint, and increase yields. From precision irrigation to soil health monitoring, our app is designed to transform agriculture into a more sustainable and efficient industry.
-				</span>
-				<a href="#" role='button' className={styles.cardButton}>
-					See More
-				</a>
-			</div>
-		</Card>,
-		<Card background='transparent' spacing='mediumSpacing'>
-			<div className={styles.cardContent}>
-				<h2 className={styles.cardTitle}>Integrated sustainability reporting</h2>
-				<span className={styles.cardDescription}>
-					We're developing a new approach to sustainable supply chains for lithium and nickel in the EV industry. By leveraging blockchain tech and strict adherence to EU regulations, we're creating a system that ensures transparency, traceability, and ethical sourcing.
-				</span>
-				<a href="#" role='button' className={styles.cardButton}>
-					See More
-				</a>
-			</div>
-		</Card>,
-	];
-	return (
-		<div className={styles.wrapper} id='ourProjects'>
-			<h2 className={styles.title}>Our Projects</h2>
-			<Carousel cards={cards} />
-		</div>
-	)
+  const cards = [
+    <Card background="transparent" spacing="mediumSpacing">
+      <div className={styles.cardContent}>
+        <h2 className={styles.cardTitle}>Agro Innovate</h2>
+        <span className={styles.cardDescription}>
+          We are developing a farm suite integrated with IoT to help farmers
+          optimize their farm processes, reduce carbon footprint, and increase
+          yields. From precision irrigation to soil health monitoring, our app
+          is designed to transform agriculture into a more sustainable and
+          efficient industry.
+        </span>
+        <a href="#" role="button" className={styles.cardButton}>
+          Learn More
+        </a>
+      </div>
+    </Card>,
+    <Card background="transparent" spacing="mediumSpacing">
+      <div className={styles.cardContent}>
+        <h2 className={styles.cardTitle}>Tide</h2>
+        <span className={styles.cardDescription}>
+          To remove floating plastic waste from targeted coastlines in Spain,
+          Portugal, Italy, and Croatia using nets and floating bin installation,
+          while educating and empowering youth through partnerships with
+          universities and high schools, turning waste into useful community
+          products.
+        </span>
+        <a href="#" role="button" className={styles.cardButton}>
+          Learn More
+        </a>
+      </div>
+    </Card>,
+    <Card background="transparent" spacing="mediumSpacing">
+      <div className={styles.cardContent}>
+        <h2 className={styles.cardTitle}>Ember</h2>
+        <span className={styles.cardDescription}>
+          To reduce human-caused wildfires in high-risk Mediterranean and
+          Eastern European regions specifically Türkiye, Bulgaria, and Greece,
+          also France by equipping communities, farmers, and youth with
+          practical wildfire prevention skills, reinforcing existing local
+          initiatives, and amplifying public awareness.
+        </span>
+        <a href="#" role="button" className={styles.cardButton}>
+          Learn More
+        </a>
+      </div>
+    </Card>,
+    <Card background="transparent" spacing="mediumSpacing">
+      <div className={styles.cardContent}>
+        <h2 className={styles.cardTitle}>Shell</h2>
+        <span className={styles.cardDescription}>
+          To protect and restore sea turtle populations by creating safe nesting
+          environments, rearing hatchlings, and strengthening biodiversity in
+          Greece (Zakynthos), Türkiye (Dalyan), and Cyprus (Lara Beach).
+        </span>
+        <a href="#" role="button" className={styles.cardButton}>
+          Learn More
+        </a>
+      </div>
+    </Card>,
+  ];
+  return (
+    <div className={styles.wrapper} id="ourProjects">
+      <h2 className={styles.title}>Projects</h2>
+      <Carousel cards={cards} />
+    </div>
+  );
 }
