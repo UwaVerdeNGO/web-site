5643a0f0a307de42bb14d7dc523e8581d30356e5
Subaga <subaga98@gmail.com>
2025-11-19 14:23:36 +0100

    Change featured news section

--- src/components/newsCard/index.tsx
+++ src/components/newsCard/index.tsx
@@ -1,20 +1,35 @@
-import React from 'react'
-import styles from "./styles.module.scss"
+import React from 'react';
+import styles from './styles.module.scss';
 
 type NewsCardProps = {
-	imageSrc: string;
-	imageAlt: string;
-	title: string;
-	category: string;
-}
-export const NewsCard: React.FC<NewsCardProps> = ({ category, imageSrc, title, imageAlt }) => {
-	return (
-		<div className={styles.wrapper}>
-			<div className={styles.imageContainer}>
-				<img src={imageSrc} alt={imageAlt} className={styles.zoomImage} />
-			</div>
-			<span className={styles.category}>{category}</span>
-			<p className={styles.title}>{title}</p>
-		</div>
-	)
-}
+  imageSrc: string;
+  imageAlt: string;
+  title: string;
+  category: string;
+
+  date: string;
+};
+export const NewsCard: React.FC<NewsCardProps> = ({
+  category,
+  imageSrc,
+  title,
+  imageAlt,
+
+  date,
+}) => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.imageContainer}>
+        <img src={imageSrc} alt={imageAlt} className={styles.zoomImage} />
+      </div>
+      <div className={styles.contentContainer}>
+        <p className={styles.title}>{title}</p>
+        <div className={styles.categoryContainer}>
+          <p className={styles.category}>{category}</p>
+          <div className={styles.dot}></div>
+          <p className={styles.date}>{date}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
