8e129275ef8e8448077719abd8d250dd424c0e78
Subaga <subaga98@gmail.com>
2025-11-19 17:20:47 +0100

    Change footer section

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,77 +1,92 @@
-import { FC } from 'react'
-import { ContentSection } from 'components/contentSection'
-import { ContentContainer } from 'components/contentSection/contentContainer'
-import { Spacer } from 'components/spacer'
-import styles from "./styles.module.scss"
-import Logo from "assets/icons/whiteLogo.png"
+import { FC } from 'react';
+import { Spacer } from 'components/spacer';
+import styles from './styles.module.scss';
+import Logo from 'assets/icons/whiteLogo.png';
+import whiteArrow from 'assets/icons/whiteArrowFoward.png';
 
 export const Footer: FC = () => {
-	return (
-		<ContentSection sectionBackground='blackBackground'>
-			<ContentContainer>
-				<Spacer className={styles.topSpacer} />
-				<div className={styles.topDivision}>
-					<div className={styles.info}>
-						<img src={Logo} alt="Logo" className={styles.logo} />
-						<p className={styles.address}>
-							Palmaille 96, 22767, Hamburg
-						</p>
-					</div>
-					<div className={styles.aboutUs}>
-						<div className={styles.aboutUsSections}>
-							<p>
-								Connect
-							</p>
-							<a href='mailto:admin@uwaverde.com' target='_blank' rel='noreferrer' id="contact-us">
-								Contact Us
-							</a>
-							{/* <a href='#' target='_blank'>
-								Careers
-							</a>
-							<a href='#' target='_blank'>
-								FAQ
-							</a> */}
-						</div>
-						{/* <div className={styles.aboutUsSections}>
-							<p>
-								Organization
-							</p>
-							<a href='#' target='_blank'>
-								About Us
-							</a>
-							<a href='#' target='_blank'>
-								Our People
-							</a>
-							<a href='#' target='_blank'>
-								Projects
-							</a>
-						</div> */}
-						<div className={styles.aboutUsSections}>
-							<p>
-								Follow Us
-							</p>
-							<a href='https://www.linkedin.com/company/uwaverde/' target='_blank' rel="noreferrer">
-								Linkedin
-							</a>
-							<a href='https://www.instagram.com/uwa.verde/' target='_blank' rel="noreferrer">
-								Instagram
-							</a>
-						</div>
-					</div>
-				</div>
+  return (
+    <div className={styles.wrapper}>
+      <Spacer className={styles.topSpacer} />
+      <div className={styles.topDivision}>
+        <div className={styles.newsLetter}>
+          <h2 className={styles.stayTitle}>Stay Ahead</h2>
+          <p className={styles.newsInfo}>
+            Subscribe for events, news, and updates
+          </p>
+          <div className={styles.inputContainer}>
+            <input
+              type="text"
+              className={styles.inputEmail}
+              placeholder="enter e-mail address"
+            />
+            <button className={styles.emailBtn}>
+              <img
+                src={whiteArrow}
+                alt="white-arrow"
+                className={styles.arrowImg}
+              />
+            </button>
+          </div>
+        </div>
+        <div className={styles.topRightDivision}>
+          <div className={styles.contact}>
+            <p className={styles.contactTitle}>Contact</p>
+            <p className={styles.contactDetails}>info@uwaverde.com</p>
+            <p className={styles.contactDetails}>
+              Palmaille 96, 22767, Hamburg Germany
+            </p>
+          </div>
+          <div className={styles.connect}>
+            <p className={styles.connectTitle}>Connect</p>
+            <a href="" className={styles.connectDetails}>
+              LinkedIn
+            </a>
+            <a href="" className={styles.connectDetails}>
+              Instagram
+            </a>
+          </div>
+          <div className={styles.discover}>
+            <p className={styles.discoverTitle}>Discover</p>
+            <a href="/about-us" className={styles.discoverDetails}>
+              About Us
+            </a>
+            <a href="#" className={styles.discoverDetails}>
+              Our People
+            </a>
+            <a href="#" className={styles.discoverDetails}>
+              Projects
+            </a>
+            <a href="#" className={styles.discoverDetails}>
+              Collaborate
+            </a>
+            <a href="#" className={styles.discoverDetails}>
+              News & Events
+            </a>
+            <a href="#" className={styles.discoverDetails}>
+              Careers
+            </a>
+          </div>
+        </div>
+      </div>
 
-				<div className={styles.bottomDivision}>
-					<p className={styles.copyRights}>
-						© 2025 UwaVerde. All Rights Reserved.
-					</p>
-					{/* <div className={styles.documents}>
-						<a href="#" className={styles.linkToDocument}>Privacy Policy</a>
-						<Spacer width='36px' />
-						<a href="#" className={styles.linkToDocument}> Terms of Use</a>
-					</div> */}
-				</div>
-				<Spacer className={styles.bottomSpacer} />
-			</ContentContainer>
-		</ContentSection>
-	)
-}
+      <div className={styles.bottomDivision}>
+        <img src={Logo} alt="logo" className={styles.logo} />
+        <div className={styles.copyRightsSection}>
+          <p className={styles.copyRights}>
+            © 2025 UwaVerde. All Rights Reserved.
+          </p>
+          <div className={styles.policyContainer}>
+            <a href="" className={styles.policyLink}>
+              Privacy Policy
+            </a>
+            <a href="" className={styles.policyLink}>
+              Terms of Use
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* <Spacer className={styles.bottomSpacer} /> */}
+    </div>
+  );
+};
