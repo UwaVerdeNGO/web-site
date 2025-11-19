ff98ff914e679af1708d069728b966dbbc0573a6
Subaga <subaga98@gmail.com>
2025-11-19 20:01:19 +0100

    Remove commented code

--- src/sections/MissionAndVision/index.tsx
+++ src/sections/MissionAndVision/index.tsx
@@ -1,36 +1,30 @@
 import React from 'react';
-
-import { Spacer } from 'components/spacer';
 import styles from './styles.module.scss';
 
 export const MissionAndVision: React.FC = () => {
   return (
-    
-        // <Spacer height="140px" className={styles.spacerSection} />
-        <div className={styles.contentGrid}>
-          <div className={styles.contentWrapperMission}>
-            <div className={styles.alignedContent}>
-              <h2 className={styles.title}>Mission</h2>
-              <p className={styles.subtext}>
-                Our mission is to help industries reduce their carbon footprint,
-                equip young people for sustainability careers, and foster
-                collaboration to achieve tangible climate outcomes.
-              </p>
-            </div>
-          </div>
-
-          <div className={styles.contentWrapperVision}>
-            <div className={styles.alignedContent}>
-              <h2 className={styles.title}>Vision</h2>
-              <p className={styles.subtext}>
-                Pioneering a future where industries embrace sustainability,
-                young people lead the charge, and zero carbon emissions pave the
-                way for a thriving planet.
-              </p>
-            </div>
-          </div>
+    <div className={styles.contentGrid}>
+      <div className={styles.contentWrapperMission}>
+        <div className={styles.alignedContent}>
+          <h2 className={styles.title}>Mission</h2>
+          <p className={styles.subtext}>
+            Our mission is to help industries reduce their carbon footprint,
+            equip young people for sustainability careers, and foster
+            collaboration to achieve tangible climate outcomes.
+          </p>
         </div>
-  )
-        // <Spacer height="140px" className={styles.spacerSection} />
-     
+      </div>
+
+      <div className={styles.contentWrapperVision}>
+        <div className={styles.alignedContent}>
+          <h2 className={styles.title}>Vision</h2>
+          <p className={styles.subtext}>
+            Pioneering a future where industries embrace sustainability, young
+            people lead the charge, and zero carbon emissions pave the way for a
+            thriving planet.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
