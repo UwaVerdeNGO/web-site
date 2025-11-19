536237292599c933283d1c2642fa6931c7bb0531
Subaga <subaga98@gmail.com>
2025-11-19 14:23:36 +0100

    Change featured news section

--- src/sections/FeaturedNews/index.tsx
+++ src/sections/FeaturedNews/index.tsx
@@ -1,25 +1,51 @@
-import { ContentSection } from 'components/contentSection'
-import { ContentContainer } from 'components/contentSection/contentContainer'
-import { NewsCard } from 'components/newsCard'
-import Mill from "assets/images/1.png"
-import PeopleWithSuits from "assets/images/2.png"
-import Bottles from "assets/images/3.png"
-import styles from "./styles.module.scss"
+import { NewsCard } from 'components/newsCard';
+import Mill from 'assets/images/mill.png';
+import PeopleWithSuits from 'assets/images/peopleWithSuits.png';
+import Bottles from 'assets/images/bottles.png';
+import styles from './styles.module.scss';
 
 export function FeaturedNews() {
-	return (
+  return (
+    <div className={styles.wrapper} id="featured-news">
+      <h2 className={styles.title}>Featured News</h2>
+      <table className={styles.cards}>
+        <tbody className={styles.cardTableBody}>
+          <tr className={styles.cardRow}>
+            <td className={styles.cardData}>
+              <NewsCard
+                category="Research paper"
+                date="October 8,2025"
+                imageAlt="Mill"
+                imageSrc={Mill}
+                title="Empowering Cities Through Wind: Evaluating the Social and Environmental Impact of Small-Scale Turbine Systems"
+              />
+            </td>
+          </tr>
 
-		<ContentSection sectionBackground='almostWhiteBackground'>
-			<ContentContainer>
-				<div className={styles.wrapper} id="featured-news">
-					<h2 className={styles.title}>Featured News</h2>
-					<div className={styles.cards}>
-						<NewsCard category='blog' imageAlt='Mill' imageSrc={Mill} title='Regenerative Agriculture: A Path to a Sustainable Future' />
-						<NewsCard category='blog' imageAlt='Plastic Bottles' imageSrc={Bottles} title='Regenerative Agriculture: A Path to a Sustainable Future' />
-						<NewsCard category='blog' imageAlt='People With Suit' imageSrc={PeopleWithSuits} title='Regenerative Agriculture: A Path to a Sustainable Future ' />
-					</div>
-				</div>
-			</ContentContainer>
-		</ContentSection>
-	)
+          <tr className={styles.cardRow}>
+            <td className={styles.cardData}>
+              <NewsCard
+                category="blog"
+                date="June 12,2025"
+                imageAlt="Plastic Bottles"
+                imageSrc={Bottles}
+                title="From Waste to Wonder: How UwaVerde Transforms Everyday Materials into Learning Toolse"
+              />
+            </td>
+          </tr>
+          <tr className={styles.cardRow}>
+            <td className={styles.cardData}>
+              <NewsCard
+                category="blog"
+                date="May 17,2025"
+                imageAlt="People With Suit"
+                imageSrc={PeopleWithSuits}
+                title="UwaVerde Launches ‘Community Climate Labs’ in Berlin and Lisbon"
+              />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
 }
