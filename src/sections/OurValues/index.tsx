c3512a5a77291c61c1da5b9752a1ef2cd44b59d5
Subaga <subaga98@gmail.com>
2025-11-21 16:28:30 +0100

    Change our values section

--- src/sections/OurValues/index.tsx
+++ src/sections/OurValues/index.tsx
@@ -4,21 +4,19 @@ import styles from './styles.module.scss';
 export function OurValues() {
   let data = TableData;
   return (
-    <>
-      <div className={styles.ourValueContainer}>
-        <h2 className={styles.valueTitle}>Our Values</h2>
-        <table className={styles.table}>
-          <tbody>
-            {data.map((item) => (
-              <tr className={styles.tableRow} key={item.id}>
-                <td className={styles.bulletNum}>{item.id}</td>
-                <td className={styles.headerData}>{item.header}</td>
-                <td className={styles.bodyData}>{item.body}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </>
+    <div className={styles.ourValueContainer}>
+      <h2 className={styles.valueTitle}>Our Values</h2>
+      <table className={styles.table}>
+        <tbody className={styles.tableBody}>
+          {data.map((item) => (
+            <tr className={styles.tableRow} key={item.id}>
+              <td className={styles.bulletNum}>{item.id}</td>
+              <td className={styles.headerData}>{item.header}</td>
+              <td className={styles.bodyData}>{item.body}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
